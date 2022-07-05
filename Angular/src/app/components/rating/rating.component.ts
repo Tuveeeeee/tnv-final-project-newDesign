4d0c381a2b78e87c349ffa64ddfe7b86b8821289
Mattia <caprasarda04@gmail.com>
2022-07-05 04:38:21 +0200

    preparando il rating

--- Angular/src/app/components/rating/rating.component.ts
+++ Angular/src/app/components/rating/rating.component.ts
@@ -5,7 +5,9 @@ import { Movie } from '../../models/movie';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ReviewService } from 'src/app/@core/services/review.service';
-import { RatingService } from '../../@core/services/rating.service';
+import { RatingService } from 'src/app/@core/services/rating.service';
+import { Review } from 'src/app/models/review';
+import { Rating } from 'src/app/models/rating';
 
 @Component({
   selector: 'tnv-rating',
@@ -19,11 +21,14 @@ export class RatingComponent implements OnInit {
     review: new FormControl('', [Validators.minLength(50), Validators.required]),
   });
 
+  rev: Partial<Review>={};
+  rat: Partial<Rating>={};
+
   movie: Partial<Movie> = {};
   id: string='';
   reviewValues: any;
 
-  constructor(private movieService: MovieService, private route: ActivatedRoute, private router: Router, private reviewService: ReviewService) { }
+  constructor(private movieService: MovieService, private route: ActivatedRoute, private router: Router, private reviewService: ReviewService, private ratingService: RatingService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -31,22 +36,28 @@ export class RatingComponent implements OnInit {
       next: (res: Partial<Movie>) => {this.movie=res},
       error: () => {this.router.navigateByUrl('/game')},
     })
+    //this.rat.userId=;
+    //this.rev.userId=;
+    this.rev.movieId=this.movie.id;
+    this.rat.movieId=this.movie.id;
   }
 
   onSubmit() {
     if(this.reviewForm.valid)
       this.reviewValues = Object.entries(this.reviewForm.value).map((x) => x[1]);
+      this.rat.rating=this.reviewValues[0];
+      this.rev.review=this.reviewValues[1];
 
       /*
-      this.reviewService.addRating(this.reviewValues[0]).subscribe({
+      this.ratingService.createRating(this.rat).subscribe({
         next: () => {this.router.navigateByUrl('/game')},
         error: () => {console.log("error")},
       });
       */
 
-      this.reviewService.addReview(this.reviewValues[1]).subscribe({
-        next: () => {this.router.navigateByUrl('/game')},
-        error: () => {console.log("error")},
-      });
+      //this.reviewService.addReview(this.rev).subscribe({
+      //  next: () => {this.router.navigateByUrl('/game')},
+      //  error: () => {console.log("error")},
+      //});
   }
 }
