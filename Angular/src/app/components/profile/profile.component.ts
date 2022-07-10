d7c6b7fb416c21a14e5505de08c24671342f5d6f
Mattia <caprasarda04@gmail.com>
2022-07-10 18:58:30 +0200

    profile pronto

--- Angular/src/app/components/profile/profile.component.ts
+++ Angular/src/app/components/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/@core/services/auth.service';
 import { User } from 'src/app/models/user';
+import { faker } from '@faker-js/faker';
+import { RatingService } from 'src/app/@core/services/rating.service';
 
 @Component({
   selector: 'tnv-profile',
@@ -9,12 +11,40 @@ import { User } from 'src/app/models/user';
 })
 export class ProfileComponent implements OnInit {
 
+  totaleRecensiti: number= 0;
   currentUser: Partial<User> = {};
+  immagineProfilo: string = '';
+  cardRedController: boolean=false;
+  cardBlueController: boolean= false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private ratingService: RatingService) { }
 
   ngOnInit(): void {
     this.currentUser = this.authService.getCurrentUser();
+    this.immagineProfilo = faker.image.cats();
+    this.trovaTotaleRecensiti(this.currentUser.id);
+    this.cardColor();
+  }
+
+  trovaTotaleRecensiti(userId: number | undefined){
+    this.ratingService.getAllRating().subscribe({
+      next: res => {
+        for( let rating of res){
+          if(rating.userId==userId){
+            this.totaleRecensiti+=1;
+          }
+        }
+      }
+    })
+  }
+
+  cardColor(){
+    if(this.currentUser.faction=='red'){
+      this.cardRedController=true;
+    }
+    else{
+      this.cardBlueController=true;
+    }
   }
 
 }
